3ea1752a6e096978acb19a42168805014c20a4b2
venkatesh1226 <venkatesh1226m@gmail.com>
2022-10-03 14:56:30 +0530

    initialised

---
+++ src/test/java/com/lti/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lti.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
