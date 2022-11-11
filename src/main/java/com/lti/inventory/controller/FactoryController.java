236feb1614ec56e96c2b4b95f72e272e37241a91
venkatesh1226 <venkatesh1226m@gmail.com>
2022-11-11 12:24:49 +0530

    cors update

--- src/main/java/com/lti/inventory/controller/FactoryController.java
+++ src/main/java/com/lti/inventory/controller/FactoryController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin
 public class FactoryController {
 
     @Autowired
--- src/main/java/com/lti/inventory/controller/ProductController.java
+++ src/main/java/com/lti/inventory/controller/ProductController.java
@@ -10,7 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin
 public class ProductController {
     @Autowired
     ProductService service;
